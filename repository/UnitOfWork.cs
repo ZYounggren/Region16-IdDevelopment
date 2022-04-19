340675e89600dd46fa224558aa01ead71d3a109f
OctaneRush <logantjones77@gmail.com>
2022-04-18 21:27:43 -0500

    Moved repository to webapp project

--- repository/UnitOfWork.cs
+++ repository/UnitOfWork.cs
@@ -1,46 +1,46 @@
-using domain;
-using domain.SpeakerAggregate;
-using domain.SessionAggregate;
-using domain.AttendeeAggregate;
-using webapp;
+// using domain;
+// using domain.SpeakerAggregate;
+// using domain.SessionAggregate;
+// using domain.AttendeeAggregate;
+// using webapp;
 
-namespace repository
-{
-    //Used from https://github.com/ahuimanu/CIDM4390/blob/5057d50a6fedc26fb249a1f87c2da9c3ff7edf97/Testing/repository/UnitOfWork.cs
-    public class UnitOfWork :IUnitOfWork
-    {
-        private readonly ApplicationDbContext _context;
-        public ISpeakerRepository Speakers { get; }
+// namespace repository
+// {
+//     //Used from https://github.com/ahuimanu/CIDM4390/blob/5057d50a6fedc26fb249a1f87c2da9c3ff7edf97/Testing/repository/UnitOfWork.cs
+//     public class UnitOfWork :IUnitOfWork
+//     {
+//         private readonly ApplicationDbContext _context;
+//         public ISpeakerRepository Speakers { get; }
 
-        public ISessionRepository Sessions { get; }
+//         public ISessionRepository Sessions { get; }
         
-        public IAttendeeRepository Attendees {get; }
+//         public IAttendeeRepository Attendees {get; }
 
-        public UnitOfWork(ApplicationDbContext applicationDbContext, 
-                          ISpeakerRepository speakersRepository, 
-                          ISessionRepository sessionsRepository,
-                          IAttendeeRepository attendeesRepository)
-        {
-            this._context = applicationDbContext;
-            this.Speakers = speakersRepository;
-            this.Sessions = sessionsRepository;
-            this.Attendees = attendeesRepository;
-        }
-        public int Complete()
-        {
-            return _context.SaveChanges();
-        }
-        public void Dispose()
-        {
-            Dispose(true);
-            GC.SuppressFinalize(this);
-        }
-        protected virtual void Dispose(bool disposing)
-        {
-            if (disposing)
-            {
-                _context.Dispose();
-            }
-        }
-    }
-}
+//         public UnitOfWork(ApplicationDbContext applicationDbContext, 
+//                           ISpeakerRepository speakersRepository, 
+//                           ISessionRepository sessionsRepository,
+//                           IAttendeeRepository attendeesRepository)
+//         {
+//             this._context = applicationDbContext;
+//             this.Speakers = speakersRepository;
+//             this.Sessions = sessionsRepository;
+//             this.Attendees = attendeesRepository;
+//         }
+//         public int Complete()
+//         {
+//             return _context.SaveChanges();
+//         }
+//         public void Dispose()
+//         {
+//             Dispose(true);
+//             GC.SuppressFinalize(this);
+//         }
+//         protected virtual void Dispose(bool disposing)
+//         {
+//             if (disposing)
+//             {
+//                 _context.Dispose();
+//             }
+//         }
+//     }
+// }
---
+++ webapp/repository/DependecyInjection.cs
@@ -0,0 +1,25 @@
+using domain;
+using domain.SpeakerAggregate;
+using domain.SessionAggregate;
+using domain.AttendeeAggregate;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace webapp.repository;
+
+    //Used from https://github.com/ahuimanu/CIDM4390/blob/5057d50a6fedc26fb249a1f87c2da9c3ff7edf97/Testing/repository/DependencyInjection.cs
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddRepository(this IServiceCollection services)
+        {
+            services.AddTransient<ISpeakerRepository, SpeakerRepository>();
+            services.AddTransient<ISessionRepository, SessionRepository>();
+            services.AddTransient<IAttendeeRepository, AttendeeRepository>();
+            services.AddTransient<IUnitOfWork, UnitOfWork>();
+
+            //services.AddDbContext<ApplicationDbContext>(options => options.UseInMemoryDatabase(databaseName: "Weather"));            
+            
+
+            return services;
+        }
+    }
