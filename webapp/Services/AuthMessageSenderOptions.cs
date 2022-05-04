23b6158cd94dbcc6a3542fdf326faad6a5d9627e
Zachery Younggren <zwyounggren1@buffs.wtamu.edu>
2022-05-04 10:18:12 -0500

    reversion

--- webapp/Services/AuthMessageSenderOptions.cs
+++ webapp/Services/AuthMessageSenderOptions.cs
@@ -2,5 +2,5 @@ namespace webapp.Services;
 
 public class AuthMessageSenderOptions
 {
-    public string SendGridKey = ("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");
+    public string? SendGridApiKey {get; set;}
 }
