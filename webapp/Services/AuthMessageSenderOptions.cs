6a864fd676c0976980fa072394e306f13dd526da
Zachery Younggren <zwyounggren1@buffs.wtamu.edu>
2022-05-03 20:27:38 -0500

    Usability fix for sendgrid key

--- webapp/Services/AuthMessageSenderOptions.cs
+++ webapp/Services/AuthMessageSenderOptions.cs
@@ -2,5 +2,5 @@ namespace webapp.Services;
 
 public class AuthMessageSenderOptions
 {
-    public string? SendGridKey { get; set; }
+    public string SendGridKey = ("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");
 }
